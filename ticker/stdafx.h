9c5b9b3edf024f169c71858e4549f2b6c6bb9ce1
d-led <dmlled@yahoo.com>
2013-02-28 23:45:23 +0100

    initial commit
    based on ppl (http://msdn.microsoft.com/en-us/library/dd984117.aspx)

---
+++ ticker/stdafx.h
@@ -0,0 +1,22 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+#include <ppl.h>
+#include <ppltasks.h>
+#include <atomic>
+
+#include <iostream>
+#include <string>
+#include <random>
+
+// TODO: reference additional headers your program requires here
---
+++ ticker/ticker.cpp
@@ -0,0 +1,93 @@
+#include "stdafx.h"
+
+using namespace concurrency;
+
+class FrequencyMeter
+{
+    std::mt19937 gen;
+	std::uniform_int_distribution<int> dis;
+public:
+	FrequencyMeter() :
+		dis(0,3),
+		gen(std::random_device()())
+	{}
+
+	int Hz()
+	{
+		auto r=rand();
+		return 60+dis(gen);
+	}
+};
+
+class active_ticker
+{
+	typedef std::function<void()> tick;
+		tick tick_once;
+		cancellation_token_source cts;
+		cancellation_token token;
+		task<void> t;
+public:
+	active_ticker(tick fun):
+		tick_once(fun),
+		token(cts.get_token())
+	{}
+public:
+	void start(unsigned interval)
+	{
+		cts=cancellation_token_source();
+		token=cts.get_token();
+		t = create_task([&,interval]
+		{
+			while (true)
+			{
+				// Check for cancellation. 
+				if (is_task_cancellation_requested())
+				{
+					cancel_current_task();
+				}
+				else 
+				{
+					wait(interval);
+					tick_once();
+				}
+			}
+		}, token);
+	}
+
+	void stop()
+	{
+		cts.cancel();
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	FrequencyMeter FM;
+
+	active_ticker measure=([&]{std::cout<<FM.Hz()<<std::endl;});
+	measure.start(250);
+
+	active_ticker ticker([]{std::cout<<"tick"<<std::endl;});
+	ticker.start(500);
+
+
+    // Wait for one second and then cancel the task.
+    wait(2000);
+
+	std::cout << "Canceling measurement ..." << std::endl;
+    measure.stop();
+	
+	wait(2000);
+
+	ticker.stop();
+
+	std::cout << "Restarting measurement ..." << std::endl;
+	measure.start(250);
+	wait(2000);
+
+	measure.stop();
+
+    std::cout << "Done" << std::endl;
+
+	return 0;
+}
